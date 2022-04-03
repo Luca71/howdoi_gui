fce7e61bed37348c4552de9be4d1ac299ed616e5
Luca71 <casagrande.posta@gmail.com>
2022-04-03 10:18:10 +0200

    Make answer editable, add option to show raw Json format and answer link

--- howdoi/core.py
+++ howdoi/core.py
@@ -3,7 +3,7 @@ import sys
 from tkinter import TOP, Toplevel
 from tkinter.font import BOLD
 import qdarkstyle
-from PySide2.QtWidgets import (QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, QWidget)
+from PySide2.QtWidgets import (QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QTextBrowser, QTextEdit)
 from PySide2.QtCore import QMargins, Qt
 from PySide2.QtGui import QFont
 from howdoi import howdoi
@@ -13,25 +13,34 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         Font = QFont('Segoe UI Variable', 12, QFont.Bold)
+        FontSmall = QFont('Segoe UI Variable', 10, QFont.Bold)
 
         self.setWindowTitle("Howdoi GUI")
         self.setFont(Font)
 
-        self.label = QLabel()
-        self.label.setFont(Font)
-        self.label.setStyleSheet("border: 1px solid #1A72BB")
-        self.label.setAlignment(Qt.AlignTop)
-
         self.input = QLineEdit()
         self.input.setFont(Font)
+
+        self.QuestioLabel = QLabel()
+        self.QuestioLabel.setFont(Font)
+        self.QuestioLabel.setText("How do I:")
+
+        self.TextEdit = QTextEdit()
+        self.TextEdit.setFont(Font)
+        self.TextEdit.setStyleSheet("border: 1px solid #1A72BB")
+        self.TextEdit.setAlignment(Qt.AlignTop)
 
         AskButton = QPushButton("Ask")
         AskButton.setFont(Font)
         AskButton.clicked.connect(self.GetAnswer)
 
-        self.QuestioLabel = QLabel()
-        self.QuestioLabel.setFont(Font)
-        self.QuestioLabel.setText("How do I:")
+        JsonButton = QPushButton("Show raw json format")
+        JsonButton.setFont(FontSmall)
+        JsonButton.clicked.connect(self.JsonAnswer)
+
+        LinkButton = QPushButton("Show the answer link")
+        LinkButton.setFont(FontSmall)
+        LinkButton.clicked.connect(self.ShowLink)
 
         HLayout = QHBoxLayout()
         HLayout.addWidget(self.QuestioLabel)
@@ -39,8 +48,10 @@ class MainWindow(QMainWindow):
 
         VLayout = QVBoxLayout()
         VLayout.addLayout(HLayout)
-        VLayout.addWidget(self.label)
         VLayout.addWidget(AskButton)
+        VLayout.addWidget(JsonButton)
+        VLayout.addWidget(self.TextEdit)
+        VLayout.addWidget(LinkButton)
 
         Container = QWidget()
         Container.setMinimumSize(500, 500)
@@ -51,9 +62,18 @@ class MainWindow(QMainWindow):
     def GetAnswer(self):
         QueryText = self.input.text()
         if QueryText != "":
-            self.label.setText(howdoi.howdoi(QueryText))
-
-
+            self.TextEdit.setText(howdoi.howdoi(QueryText))
+    
+    def JsonAnswer(self):
+        QueryText = self.input.text()
+        if QueryText != "":
+            self.TextEdit.setText(howdoi.howdoi(QueryText + " -j"))
+    
+    def ShowLink(self):
+        QueryText = self.input.text()
+        if QueryText != "":
+            self.TextEdit.setText(howdoi.howdoi(QueryText + " -l"))
+            
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyside2()) # usign dark mode stylesheet
